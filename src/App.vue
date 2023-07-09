a1867d38d4757958f74ec8c5b05b60efc9653811
Nik <nikremenskovs@gmail.com>
2023-07-09 20:12:57 +0100

    tailwind prettier lugin installed

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">Home</router-link> |
+      <router-link class="flex flex-col text-4xl text-white" to="/"
+        >Home</router-link
+      >
+      |
       <router-link to="/about">About</router-link>
     </nav>
     <router-view />
