87d44c2c166736b10ac8a8bce9865a9088e46603
Nik <nikremenskovs@gmail.com>
2023-07-09 20:26:08 +0100

    eslint

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,21 +1,27 @@
 <template>
-    <section class="dashboard min-h-screen flex flex-col justify-center">
-        <div class="dashboard__text mb-16">
-            <h1 class="font-yeseva-one text-2xl mb-8 tracking-wider">Welcome to your Dashboard</h1>
-            <p class="font-marmelad text-base  tracking-wide">Here you can navigate your tasks or create a new one!</p>
+  <section class="dashboard flex min-h-screen flex-col justify-center">
+    <div class="dashboard__text mb-16">
+      <h1 class="font-yeseva-one mb-8 text-2xl tracking-wider">
+        Welcome to your Dashboard
+      </h1>
+      <p class="font-marmelad text-base tracking-wide">
+        Here you can navigate your tasks or create a new one!
+      </p>
+    </div>
+    <ul class="dashboard-items">
+      <li
+        v-for="n in 4"
+        class="dashboard-items__all bg-todo-secondary mb-8 flex h-24 w-full items-center justify-between rounded-xl shadow-xl"
+      >
+        <div class="flex">
+          <div class="bg-todo-red h-12 w-12" />
+          <div>
+            <h2>All Tasks</h2>
+            <p>Tasks: 0</p>
+          </div>
         </div>
-        <ul class="dashboard-items">
-            <li v-for="n in 4" class="dashboard-items__all w-full h-24 mb-8 bg-todo-secondary rounded-xl shadow-xl flex justify-between items-center">
-                <div class="flex">
-
-                    <div class="h-12 w-12 bg-todo-red" />
-                    <div>
-                        <h2>All Tasks</h2>
-                        <p>Tasks: 0</p>
-                    </div>
-                </div>
-                <div class="h-12 w-12 bg-todo-green" />
-            </li>  
-        </ul>
-    </section>
+        <div class="bg-todo-green h-12 w-12" />
+      </li>
+    </ul>
+  </section>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <template>
-    <section class="homepage flex h-screen flex-col justify-center text-center items-center text-todo-primary">
-        <!-- make the image responsive by utilising picture tag -->
-        <img class="homepage__image rounded-full h-64 w-64 mb-12" src="@/assets/images/dashboardImage.jpg" alt="Homepage image with a notebook that contains a simple four step to do list where the first three items are ticked. This suggests that whoever made this list has already ticked 'Wake up', 'Make Coffee', 'Drink Coffee'. The last untiked item reads'Make more coffee">
-        <h1 class="homepage__heading font-yeseva-one text-2xl mb-8 tracking-wider">ECD To Do Assistant</h1>
-        <p class="homepage__paragraph font-marmelad text-base mb-12 tracking-wide">Hello, ECD Limited colleague! Stay on top of your day and maximize productivity with our all-in-one to-do app, designed to streamline your tasks, prioritize goals, and ensure a successful and fulfilling day</p>
-        <router-link class="homepage__CTA" to="/dashboard">GET STARTED</router-link>
-    </section>
+  <section
+    class="homepage text-todo-primary flex h-screen flex-col items-center justify-center text-center"
+  >
+    <!-- make the image responsive by utilising picture tag -->
+    <img
+      class="homepage__image mb-12 h-64 w-64 rounded-full"
+      src="@/assets/images/dashboardImage.jpg"
+      alt="Homepage image with a notebook that contains a simple four step to do list where the first three items are ticked. This suggests that whoever made this list has already ticked 'Wake up', 'Make Coffee', 'Drink Coffee'. The last untiked item reads'Make more coffee"
+    />
+    <h1 class="homepage__heading font-yeseva-one mb-8 text-2xl tracking-wider">
+      ECD To Do Assistant
+    </h1>
+    <p class="homepage__paragraph font-marmelad mb-12 text-base tracking-wide">
+      Hello, ECD Limited colleague! Stay on top of your day and maximize
+      productivity with our all-in-one to-do app, designed to streamline your
+      tasks, prioritize goals, and ensure a successful and fulfilling day
+    </p>
+    <router-link class="homepage__CTA" to="/dashboard">GET STARTED</router-link>
+  </section>
 </template>
